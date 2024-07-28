4d3832f98a1f3e4270602129ad6bd5c557905013
DragonJ <imdragonmj@gmail.com>
2024-07-28 20:20:04 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ import Services from './component/Services';
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
+    <div>Monish</div>
       <div className="max-w-6xl mx-auto bg-white p-6 rounded-lg shadow-lg">
         <h1 className="text-2xl font-bold mb-6">Services</h1>
         <Services />
